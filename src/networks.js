f71ca178c150ee0c2a728a22d18682566624bb62
constellationvault <14243318+NicoDFS@users.noreply.github.com>
2022-09-12 11:09:49 -0400

    Network

--- src/networks.js
+++ src/networks.js
@@ -6,7 +6,7 @@ module.exports = {
             public: 0x0488b21e,
             private: 0x0488ade4
         },
-        pubKeyHash: 0x3a,
+        pubKeyHash: 0x17,
         scriptHash: 0x32,
         wif: 0x80
     },
@@ -17,7 +17,7 @@ module.exports = {
             public: 0x043587cf,
             private: 0x04358394
         },
-        pubKeyHash: 0x78,
+        pubKeyHash: 0x58,
         scriptHash: 0x6e,
         wif: 0xef
     }
